36f2c1de2e3d6ceb57b83d6601113e74a0aec6c7
YUSUKE SAKAKIBARA <ysakakib@e1z1r6p40.42.us.org>
2017-01-21 20:00:20 -0800

    fix match

--- ex00/match.c
+++ ex00/match.c
@@ -10,20 +10,22 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int		match(char *s1, char *s2)
+int	match(char *s1, char *s2)
 {
-
-	if (*s1 == '\0' || *s2 == '\0')
+	if (!(*s1) && !(*s2))
+		return (1);
+	if (*s2 == '*')
 	{
-		if (*s1 == '\0' && *s2 == '\0')
-			return (1);
-		return (0);
+		if (!(*s1))
+			return (match(s1, s2 + 1));
+		if (*s1)
+			return (match(s1, s2 + 1) || match(s1 + 1, s2));
 	}
-
+	if (!(*s1) || !(*s2))
+		return (0);
+	if (*s1 != *s2)
+		return (0);
 	if (*s1 == *s2)
 		return (match(s1 + 1, s2 + 1));
-	else if (*s2 == '*')
-		return (match(s1 + 1, s2) || match(s1 + 1, s2 + 1));
-	else
-		return (0);
+	return (-1);
 }
